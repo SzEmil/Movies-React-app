7c854120572c78ecf0f74cb30648b3967ec1bbe3
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-02-26 01:17:02 +0100

    lil update of logo

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,13 +1,18 @@
 import { NavLink, Outlet } from 'react-router-dom';
 import clsx from 'clsx';
 import css from './SharedLayout.module.css';
+
 export const SharedLayout = () => {
   return (
     <div>
       <header className={clsx(css.headerContainer)}>
         <nav className={clsx(css.nav)}>
           <NavLink to="/" className={clsx(css.logo)}>
-            <span style={{ color: 'red' }}>FILM</span>OTEKA
+            <img
+              className={clsx(css.logoImg)}
+              src="https://scontent.xx.fbcdn.net/v/t1.15752-9/331877923_577736621071333_4370968066276411511_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_ohc=Jgiozd7hq38AX-oIhR2&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTrVORXxu5jxBgxSbId8NoNckfLD3Mvid60DTzdv6xJMw&oe=6422129C"
+              alt="logo pic"
+            />
           </NavLink>
           <NavLink to="/" className={clsx(css.navLink)}>
             HOME
